44a5085c5d9b0f1d23593eafcf56aa57175e5505
abhi-nokhwal <abhinokhwal461@gmail.com>
2024-05-03 11:58:31 +0530

    v12

---
+++ index.js
@@ -0,0 +1,14 @@
+
+let inc = () => {
+    document.getElementById('bd').style.backgroundColor = ' chocolate';
+}
+let gnc = () => {
+    document.getElementById('bd').style.backgroundColor = 'red';
+}
+let pnc = () => {
+    document.getElementById('bd').style.backgroundColor = 'blue';
+}
+
+
+
+ 
